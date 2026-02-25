5ea2536c6f5a5fd835640d8a0626b4003978d5e3
zfwillis <113203439+zfwillis@users.noreply.github.com>
2026-02-25 17:23:58 -0500

    HTML form complete and fixed multiple prizes buttons

--- prizes.js
+++ prizes.js
@@ -1,7 +1,11 @@
 function showPrizes() {
     document.getElementById("prizes").setAttribute("style", "visibility:visible;display:block");
+    document.getElementById("showBtn").setAttribute("style", "visibility:hidden");
+    document.getElementById("hideBtn").setAttribute("style", "visibility:visible");
 }
 
 function hidePrizes() {
     document.getElementById("prizes").setAttribute("style", "visibility:hidden;display:none");
+    document.getElementById("showBtn").setAttribute("style", "visibility:visible");
+    document.getElementById("hideBtn").setAttribute("style", "visibility:hidden");
 }
